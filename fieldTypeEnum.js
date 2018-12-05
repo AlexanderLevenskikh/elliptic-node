fce5ef91032f25d7d211e5eb5c876f68e975bc04
Левенских Александр Андреевич <levenskikh@skbkontur.ru>
2018-12-05 22:51:54 +0500

    remove js

--- fieldTypeEnum.js
+++
@@ -1,5 +0,0 @@
-export var FieldTypeEnum;
-(function (FieldTypeEnum) {
-    FieldTypeEnum[FieldTypeEnum["prime"] = 0] = "prime";
-    FieldTypeEnum[FieldTypeEnum["bit"] = 1] = "bit";
-})(FieldTypeEnum || (FieldTypeEnum = {}));
