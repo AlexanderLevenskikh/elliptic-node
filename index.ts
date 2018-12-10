4e428659043c6ff9ad13bec0e26f4e13a48e1363
Левенских Александр Андреевич <levenskikh@skbkontur.ru>
2018-12-11 02:44:39 +0500

    Prime case

--- index.ts
+++ index.ts
@@ -7,11 +7,14 @@ import { Curve } from './curve/curve';
 import { FieldTypeEnum } from './fieldTypeEnum';
 import { ProgramInput } from './inputInterface';
 import bigInt = require('big-integer');
+import { Point } from './point/point';
 
 const input = require('./input.json');
 
 (() => {
     const { module } = input;
+
+    let result;
 
     if (Array.isArray(module)) {
         const inputBinary = <ProgramInput<number[]>> input;
@@ -54,7 +57,7 @@ const input = require('./input.json');
                 return;
             }
 
-            const result = point.multiply(factorBigInt, aPolynomial);
+            result = point.multiply(factorBigInt, aPolynomial);
         } else {
             let pointA, pointB;
 
@@ -84,8 +87,7 @@ const input = require('./input.json');
                 return;
             }
 
-            const result = pointA.add(pointB, aPolynomial);
-            console.log(result);
+            result = pointA.add(pointB, aPolynomial);
         }
     } else if (typeof module === 'string') {
         const inputPrime = <ProgramInput<string>> input;
@@ -106,9 +108,62 @@ const input = require('./input.json');
             bCoordinate,
             moduleCoordinate,
         );
+
+        // Умножение
+        if (factor) {
+            const factorBigInt = bigInt(factor);
+
+            let point;
+
+            if (!x || !y) {
+                point = Point.prototype.infinityPointFactory();
+            } else {
+                const xCoordinate = new Coordinate(x);
+                const yCoordinate = new Coordinate(y);
+
+                point = new Point(xCoordinate, yCoordinate, moduleCoordinate, false);
+            }
+
+            if (!curve.isPointLiesOnCurve(point)) {
+                console.error('Точка не лежит на кривой.');
+                return;
+            }
+
+            result = point.multiply(factorBigInt, aCoordinate);
+        } else {
+            let pointA, pointB;
+
+            if (!x1 || !y1) {
+                pointA = Point.prototype.infinityPointFactory();
+            } else {
+                const x1Coordinate = new Coordinate(x1);
+                const y1Coordinate = new Coordinate(y1);
+
+                pointA = new Point(x1Coordinate, y1Coordinate, moduleCoordinate, false);
+            }
+            if (!curve.isPointLiesOnCurve(pointA)) {
+                console.error('Точка А не лежит на кривой.');
+                return;
+            }
+
+            if (!x2 || !y2) {
+                pointB = Point.prototype.infinityPointFactory();
+            } else {
+                const x2Coordinate = new Coordinate(x2);
+                const y2Coordinate = new Coordinate(y2);
+
+                pointB = new Point(x2Coordinate, y2Coordinate, moduleCoordinate, false);
+            }
+            if (!curve.isPointLiesOnCurve(pointB)) {
+                console.error('Точка В не лежит на кривой.');
+                return;
+            }
+
+            result = pointA.add(pointB, aCoordinate);
+        }
     }
 
-    fs.writeFile('output.txt', 'Hello Node.js', (err) => {
+    fs.writeFile('output.txt', result ? result.toString() : '', (err) => {
         if (err) throw err;
         console.log('It\'s saved!');
     });
